d446372cc6772b445374695ab9a02181c8ccbb99
julisr <julietarpo3@gmail.com>
2025-07-02 21:50:32 -0300

    Update Context.jsx

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -1,29 +1,34 @@
-import { createContext , useState} from "react";
+import { createContext, useState } from "react";
 
-export const Context = createContext()
+
+export const Context = createContext();
 
 export const ContextProvider = ({ children }) => {
-    const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
-const buyProducts = (product) => {
-  const productRepeat = cart.find((item) => item.id === product.id);
-  
-  if (productRepeat) {
-    setCart(cart.map((item) => 
-      item.id === product.id 
-      ? { ...item, quanty: item.quanty + 1 }
-      : item
-    ));
-  } else {
-    setCart([...cart, { ...product, quanty: 1 }]);
-  }
-};
+  const buyProducts = (product) => {
+    const productRepeat = cart.find((item) => item.id === product.id);
 
+    if (productRepeat) {
+      if (productRepeat.quanty < 5) {
+        setCart(
+          cart.map((item) =>
+            item.id === product.id
+              ? { ...item, quanty: item.quanty + 1 }
+              : item
+          )
+        );
 
-    return (
-        <Context.Provider value={{cart, setCart, buyProducts}}>
-            {children}
-        </Context.Provider>
-    )
-}
+      }
+    } else {
+      setCart([...cart, { ...product, quanty: 1 }]);
+    }
+  };
 
+  return (
+    <Context.Provider value={{ cart, setCart, buyProducts }}>
+      {children}
+    </Context.Provider>
+  );
+
+  };
